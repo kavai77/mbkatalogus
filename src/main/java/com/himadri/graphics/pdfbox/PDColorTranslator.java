811d7b2d619d13948a6c3bafc3296b6da42f7c89
kavai77 <kavai.csaba@zoho.com>
2017-08-23 09:21:41 -0400

    fixing colors

--- src/main/java/com/himadri/graphics/pdfbox/PDColorTranslator.java
+++ src/main/java/com/himadri/graphics/pdfbox/PDColorTranslator.java
@@ -2,26 +2,43 @@ package com.himadri.graphics.pdfbox;
 
 import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
 import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceCMYK;
+import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceRGB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
 
 @Component
 public class PDColorTranslator {
-    public PDColor translatePDColor(Color color) {
-        if (color == null)
-            return new PDColor(new float[] { 0, 0, 0, 1f }, PDDeviceCMYK.INSTANCE);
+    private static final Logger LOG = LoggerFactory.getLogger(PDColorTranslator.class);
 
-        float[] c = color.getRGBColorComponents(null);
-        float k = 1 - Math.max(c[0], Math.max(c[1], c[2]));
-        if (k == 1) {
-            return new PDColor(new float[]{0, 0, 0, 1}, PDDeviceCMYK.INSTANCE);
-        } else {
-            return new PDColor(new float[]{
-                    (1 - c[0] - k) / (1 - k),
-                    (1 - c[1] - k) / (1 - k),
-                    (1 - c[2] - k) / (1 - k),
-                    k}, PDDeviceCMYK.INSTANCE);
+    public static final float GRAY_THRESHOLD = 0.01f;
+
+    public PDColor translatePDColor(Color color) {
+        if (color == null) {
+            return new PDColor(new float[]{0, 0, 0, 1f}, PDDeviceCMYK.INSTANCE);
         }
+
+        final float[] rgb = color.getRGBColorComponents(null);
+        final float max = Math.max(rgb[0], Math.max(rgb[1], rgb[2]));
+        if (max == 0) {
+            return new PDColor(new float[]{0, 0, 0, 1}, PDDeviceCMYK.INSTANCE);
+        } else if (isGray(rgb)){
+            return new PDColor(new float[]{
+                    (max - rgb[0]) / max,
+                    (max - rgb[1]) / max,
+                    (max - rgb[2]) / max,
+                    1 - max}, PDDeviceCMYK.INSTANCE);
+        } else {
+            return new PDColor(rgb, PDDeviceRGB.INSTANCE);
+        }
+    }
+
+    private boolean isGray(float[] rgb) {
+        final float avg = (rgb[0] + rgb[1] + rgb[2]) / 3;
+        return Math.abs(avg - rgb[0]) < GRAY_THRESHOLD &&
+               Math.abs(avg - rgb[1]) < GRAY_THRESHOLD &&
+               Math.abs(avg - rgb[2]) < GRAY_THRESHOLD;
     }
 }
--- src/main/java/com/himadri/renderer/Util.java
+++ src/main/java/com/himadri/renderer/Util.java
@@ -4,6 +4,7 @@ import com.himadri.graphics.pdfbox.PDFontService;
 import com.himadri.graphics.pdfbox.PdfBoxGraphics;
 import com.himadri.model.rendering.Box;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,17 +17,28 @@ public class Util {
     private PDFontService pdFontService;
 
     private static final Color[] PRODUCT_GROUP_COLORS = new Color[] {
-            new Color(176, 208, 228),
-            new Color(237, 162, 143),
-            new Color(142, 180, 193),
-            new Color(254, 240, 223),
-            new Color(149, 176, 153)
+            new Color(124, 171, 185),
+            new Color(157, 200, 223),
+            new Color(229, 198, 144),
+            new Color(239, 156, 138),
+            new Color(139, 177, 176),
+            new Color(245, 148, 49),
+            new Color(253, 236, 215)
     };
-    private PDDocument pdDocument = new PDDocument();
 
     public int getStringWidth(PdfBoxGraphics g2, String text) {
         try {
             return Math.round(g2.getFont().getStringWidth(text) / 1000f * g2.getFontSize());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getStringWidth(Font font, String text) {
+        try {
+            final PDDocument pdDocument = new PDDocument();
+            final PDFont pdFont = pdFontService.getPDFont(pdDocument, font);
+            return Math.round(pdFont.getStringWidth(text) / 1000f * font.getSize2D());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
